88d73e686d65a5d1a34d48a8e389a5813bf8ef66
komikun <z1632259373@gmail.com>
2021-09-05 18:50:46 +0800

    test connection

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module gobot
 
 go 1.15
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	log_path string
+)
 
 func main() {
-	fmt.Println("vim-go")
+	app := &cli.App{
+		Name:    "gobot",
+		Usage:   "QQ Bot in golang",
+		Version: "1.0.0.1",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "log",
+				Aliases: []string{"l"},
+				Usage:   "config log file path",
+				Value:   "./log",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			return nil
+		},
+	}
+	app.Run(os.Args)
 }
