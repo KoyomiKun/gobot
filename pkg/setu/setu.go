3f9442e0a143916ad6276cded2be432ac2ee47a4
komikun <z1632259373@gmail.com>
2021-09-30 23:36:06 +0800

    finish setu module

--- pkg/setu/setu.go
+++ pkg/setu/setu.go
@@ -2,8 +2,10 @@ package setu
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	zero "github.com/wdvxdr1123/ZeroBot"
@@ -12,30 +14,16 @@ import (
 )
 
 const (
-	baseUrl string = "https://api.lolicon.app/setu/v2"
+	baseUrl       string = "https://api.lolicon.app/setu/v2"
+	msgInfoFormat        = "\n%s : %s,"
 )
 
 func init() {
 	zero.OnCommand("色图").Handle(func(ctx *zero.Ctx) {
-		var cmd extension.CommandModel
-		err := ctx.Parse(&cmd)
-		if err != nil {
-			log.Errorf("fail parse ctx: %s", ctx.Event.RawMessage)
-			return
-		}
-
-		if cmd.Args == "" {
-			ctx.Send("请附带关键字参数")
-			return
-		}
-
-		setu := getSetu(cmd.Args)
-		if setu == nil {
-			return
-		}
-
-		ctx.Send(message.At(ctx.Event.UserID))
-		ctx.Send(message.Image(setu.Urls["origin"]))
+		parseSetu(ctx, false)
+	})
+	zero.OnCommand("R18色图").Handle(func(ctx *zero.Ctx) {
+		parseSetu(ctx, true)
 	})
 }
 
@@ -59,7 +47,56 @@ type Resp struct {
 	Data []*Data `json:"data,omitempty"`
 }
 
-func getSetu(keyword string) *Data {
+func parseSetu(ctx *zero.Ctx, r18 bool) {
+
+	var cmd extension.CommandModel
+	err := ctx.Parse(&cmd)
+	if err != nil {
+		log.Errorf("fail parse ctx: %s", ctx.Event.RawMessage)
+		return
+	}
+
+	if cmd.Args == "" {
+		ctx.Send("请附带关键字参数")
+		return
+	}
+
+	go ctx.Send(
+		message.Message{
+			message.At(ctx.Event.UserID),
+			message.Text("开始查找..."),
+		},
+	)
+
+	setu := getSetu(cmd.Args, r18)
+	if setu == nil {
+		ctx.Send(message.Text("查找图片失败"))
+		return
+	}
+	if len(setu) == 0 {
+		ctx.Send(message.Text("该关键词下搜不到图片"))
+		return
+	}
+
+	topSetu := setu[0]
+	picUrl, ok := topSetu.Urls["original"]
+	if !ok {
+		ctx.Send(message.Text("Key解析失败，请联系管理员处理"))
+		return
+	}
+
+	var msgInfoBuilder strings.Builder
+	msgInfoBuilder.WriteString(fmt.Sprintf(msgInfoFormat, "标题", topSetu.Title))
+	msgInfoBuilder.WriteString(fmt.Sprintf(msgInfoFormat, "作者", topSetu.Author))
+	msgInfoBuilder.WriteString(fmt.Sprintf(msgInfoFormat, "标签", strings.Join(topSetu.Tags, ",")))
+
+	ctx.Send(message.Message{
+		message.At(ctx.Event.UserID),
+		message.Text(msgInfoBuilder.String()),
+		message.Image(picUrl),
+	})
+}
+func getSetu(keyword string, r18 bool) []*Data {
 
 	client := &http.Client{}
 
@@ -70,7 +107,11 @@ func getSetu(keyword string) *Data {
 	}
 	query := req.URL.Query()
 	query.Add("keyword", keyword)
-	query.Add("r18", "1")
+	if r18 {
+		query.Add("r18", "1")
+	} else {
+		query.Add("r18", "0")
+	}
 	req.URL.RawQuery = query.Encode()
 
 	resp, err := client.Do(req)
@@ -92,5 +133,5 @@ func getSetu(keyword string) *Data {
 		log.Errorf("fail getting setu: %s", respBody.Err)
 		return nil
 	}
-	return respBody.Data[0]
+	return respBody.Data
 }
