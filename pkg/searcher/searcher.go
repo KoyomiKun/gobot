6b68a2c961bb4904665503e85998afb00d2aa1cf
komikun <z1632259373@gmail.com>
2021-09-30 23:36:06 +0800

    finish setu

--- pkg/searcher/searcher.go
+++ pkg/searcher/searcher.go
@@ -3,7 +3,6 @@ package searcher
 import zero "github.com/wdvxdr1123/ZeroBot"
 
 func init() {
-	zero.OnCommand("看图").Handle(func(ctx *zero.Ctx) {
-		ctx.Send("不行")
+	zero.OnCommand("查图").Handle(func(ctx *zero.Ctx) {
 	})
 }
