ce2ce624a7e685186219baf8b12e1225cc6763f4
komikun <z1632259373@gmail.com>
2021-10-01 09:20:39 +0800

    test

--- pkg/searcher/searcher.go
+++ pkg/searcher/searcher.go
@@ -1,8 +1,18 @@
 package searcher
 
-import zero "github.com/wdvxdr1123/ZeroBot"
+import (
+	log "github.com/sirupsen/logrus"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/extension"
+)
 
 func init() {
 	zero.OnCommand("查图").Handle(func(ctx *zero.Ctx) {
+		var cmd extension.CommandModel
+		err := ctx.Parse(&cmd)
+		if err != nil {
+			log.Errorf("fail parse ctx: %s", ctx.Event.RawMessage)
+			return
+		}
 	})
 }
